b622666ae2ddfdb7fb7f287aa754cafb79b45d3f
Marta Kulig <bialka104b@interia.pl>
2020-08-27 15:50:42 +0200

    163.Public API, method fetch().then() -v1

--- API-fetch-promises-project-/js/app.js
+++ API-fetch-promises-project-/js/app.js
@@ -1,12 +1,55 @@
-fetch("https://www.googleapis.com/books/v1/volumes?q=quilting")
-  .then((response) => {
-    //return response.text(); //metoda do odkodowania naszego responsa, zwraca ona kolejnego
-    return response.json(); //metoda podobna do text z tym że rozkodowuje nam jeszcze jsona do obiektu JS
-  })
-  .then((booksInfo) => {
-    console.log(booksInfo);
-    //wszystko tutaj zostanie zwrócone jako json dzięki metodzie response.text()
-  })
-  .catch((error) => {
-    console.log("Niestety masz błąd: ", error);
-  });
+function listBreeds() {
+  return fetch("https://dog.ceo/api/breeds/list/all") //tu dostajemy liste obrazków
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.message;
+    })
+    .catch((err) => {
+      console.log("coś poszło nie tak", err);
+    });
+}
+
+// listBreeds().then((breeds) => {
+//   console.log(breeds);
+// });
+
+function getRandomImage() {
+  return fetch("https://dog.ceo/api/breeds/image/random") //tu losujemy losowy obrazek
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.message;
+    })
+    .catch((err) => {
+      console.log("coś poszło nie tak", err);
+    });
+}
+
+// const imgTag = document.querySelector("img");
+// getRandomImage().then((imgSrc) => {
+//   imgTag.setAttribute("src", imgSrc);
+// });
+
+function getRandomImageByBreed(breed) {
+  return fetch(`https://dog.ceo/api/breed/${breed}/images/random`) //tu losujemy losowy obrazek z kategorii husky
+    .then((response) => {
+      return response.json();
+      console.log(`https://dog.ceo/api/breed/${breed}/images/random`);
+    })
+    .then((data) => {
+      return data.message;
+    })
+    .catch((err) => {
+      console.log("coś poszło nie tak", err);
+    });
+}
+
+const imgTag = document.querySelector("img");
+
+getRandomImageByBreed("husky").then((imgSrc) => {
+  //wywołanie obrazka/ jako argument trzeba podać breed z dokumentacji w naszym przypadku "husky"
+  imgTag.setAttribute("src", imgSrc);
+});
