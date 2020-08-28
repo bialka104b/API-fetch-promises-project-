238ace58a615b4250143f70f0d274f5775715ac9
Marta Kulig <bialka104b@interia.pl>
2020-08-28 12:01:54 +0200

    merge

---
+++ API-fetch-promises-project-/API-fetch-promises-project-/js/app.js
@@ -0,0 +1,119 @@
+class Dogs {
+  constructor() {
+    this.apiURL = "https://dog.ceo/api";
+    this.imgTag = document.querySelector(".featured-dog img"); //wybieram img o klasie featured-dog
+    this.bacgroundTag = document.querySelector(".featured-dog__background");
+    this.tilesElement = document.querySelector(".tiles");
+    this.tilesElement.textContent = "";
+    this.spinnerElement = document.querySelector(".spinner");
+    this.init();
+  }
+
+  showLoading() {
+    this.spinnerElement.classList.add("spinner--visible");
+  }
+  hideLoading() {
+    this.spinnerElement.classList.remove("spinner--visible");
+  }
+  listBreeds() {
+    return fetch(`${this.apiURL}/breeds/list/all`) //tu dostajemy liste obrazków
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data.message;
+      })
+      .catch((err) => {
+        console.log("coś poszło nie tak", err);
+      });
+  }
+
+  getRandomImage() {
+    return fetch(`${this.apiURL}/breeds/image/random`) //tu losujemy losowy obrazek
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data.message;
+      })
+      .catch((err) => {
+        console.log("coś poszło nie tak", err);
+      });
+  }
+
+  getRandomImageByBreed(breed) {
+    return fetch(`${this.apiURL}/breed/${breed}/images/random`) //tu losujemy losowy obrazek z kategorii husky
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data.message;
+      })
+      .catch((err) => {
+        console.log("coś poszło nie tak", err);
+      });
+  }
+
+  init() {
+    this.showLoading();
+    this.getRandomImage().then((src) => {
+      this.imgTag.setAttribute("src", src);
+      this.bacgroundTag.style.background = `url("${src}")`;
+      this.hideLoading();
+    });
+
+    this.showAllBreeds();
+  }
+
+  addBreed(breed, subBreed) {
+    let name;
+    let type;
+    if (typeof subBreed === "undefined") {
+      name = breed;
+      type = breed;
+    } else {
+      name = `${breed} ${subBreed}`;
+      type = `${breed}/${subBreed}`; //tu musi być ukośnik i ez spacji bo to idzie do urla
+    }
+    const tile = document.createElement("div");
+    tile.classList.add("tiles__tile");
+
+    const tileContent = document.createElement("div");
+    tileContent.classList.add("tiles__tile-content");
+
+    tileContent.innerText = name;
+    tileContent.addEventListener("click", () => {
+      window.scrollTo(0,0);
+      this.showLoading();
+      
+      this.getRandomImageByBreed(type).then((src) => {
+        this.imgTag.setAttribute("src", src);
+        this.bacgroundTag.style.background = `url("${src}")`;
+        this.hideLoading();
+      });
+    });
+    tile.appendChild(tileContent);
+    this.tilesElement.appendChild(tile);
+  }
+
+  showAllBreeds() {
+    this.listBreeds().then((breeds) => {
+      //pobranie listy
+      for (const breed in breeds) {
+        if (breeds[breed].length === 0) {
+          //jeżeli tablica breedów ma 0 długość
+          this.addBreed(breed); //to wypisujemy tylko nazwe tablicy
+        } else {
+          for (const subBreed of breeds[breed]) {
+            this.addBreed(breed, subBreed);
+            // console.log(`${breed}: ${subBreed}`); //jeśli nie to wypisujemy również obiekty w tablicy
+          }
+        }
+      }
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log(new Dogs(event));
+});
